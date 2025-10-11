1f78d9febef0d77cfe63eba6c21d14cac0991853
Kot <blackcourse@mail.ru>
2025-10-11 11:15:40 +0300

    Added results and response

--- src/test/java/tests/TestAutomationPracticeForm.java
+++ src/test/java/tests/TestAutomationPracticeForm.java
@@ -14,7 +14,7 @@ public class TestAutomationPracticeForm {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -26,16 +26,28 @@ public class TestAutomationPracticeForm {
         $x("//label[@for='gender-radio-1']").click();
         $("#userNumber").setValue("8999562458");
         $("#dateOfBirthInput").click();
-        $x("//option[@value='2014']").click();
-        $x("//option[@value='5']").click();
-        $(".react-datepicker__day--024").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__day.react-datepicker__day--010").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $x("//label[@for='hobbies-checkbox-2']").click();
+        $("#uploadPicture").uploadFromClasspath("img.png");
         $("#currentAddress").setValue("Another street 1");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
-
         $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("div.table-responsive").shouldHave(text("Alex Bolduin"));
+        $("div.table-responsive").shouldHave(text("abld@nail.ru"));
+        $("div.table-responsive").shouldHave(text("Male"));
+        $("div.table-responsive").shouldHave(text("8999562458"));
+        $("div.table-responsive").shouldHave(text("10 June,1990"));
+        $("div.table-responsive").shouldHave(text("img.png"));
+        $("div.table-responsive").shouldHave(text("Another street 1"));
+        $("div.table-responsive").shouldHave(text("NCR Delhi"));
 
 
     }
