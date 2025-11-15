8be66247e81df35fe37d29cbb5715d729e509d51
Kot <blackcourse@mail.ru>
2025-11-15 19:58:32 +0300

    Addded alluretest

---
+++ src/test/java/guru/qa/allure/GitHub.java
@@ -0,0 +1,28 @@
+package guru.qa.allure;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import tests.TestBase;
+
+import static com.codeborne.selenide.CollectionCondition.anyMatch;
+
+import static com.codeborne.selenide.Selenide.*;
+
+class SeleinideTest extends TestBase {
+
+    @Test
+    @Owner("Blackcourse")
+    @Severity(SeverityLevel.BLOCKER)
+    void simpleSeleinideTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("/Blackcourse/Auto_test_demoqa");
+        $("#issues-tab").click();
+        $$("[data-testid=issue-pr-title-link]").shouldHave(anyMatch(
+                "element exist",
+                el -> el.getText().contains("Bug2")
+        ));
+    }
+}
---
+++ src/test/java/guru/qa/allure/GitHubSteps.java
@@ -0,0 +1,33 @@
+package guru.qa.allure;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.CollectionCondition.anyMatch;
+
+public class GitHubSteps {
+    final private SelenideElement issueTab = $("#issues-tab");
+    final private ElementsCollection bugByNameField = $$("[data-testid=issue-pr-title-link]");
+
+    @Step("Open github repo")
+    public void openRepoPage(String link) {
+        open(link);
+    }
+
+    @Step("Click on \"Issue\" tab")
+    public void openIssueTab() {
+        issueTab.click();
+    }
+
+    @Step("Check that bug with {name} exists")
+    public void checkBugByName(String name) {
+        bugByNameField.shouldHave(anyMatch(
+                "element exist",
+                el -> el.getText().contains(name)
+        ));
+    }
+}
